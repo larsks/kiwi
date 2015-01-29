e2c9b5a8f88d2ea2345cbe9db0ae06a2d6b257ff
Lars Kellogg-Stedman <lars@redhat.com>
2015-01-29 16:47:47 -0500

    more exceptions in prep for manager.py overhaul

--- kiwi/exc.py
+++ kiwi/exc.py
@@ -1,13 +1,32 @@
 class KiwiError (Exception):
-    pass
+    def __init__(self, message, reason=None, returncode=None,
+                 stdout=None, stderr=None, *args, **kwargs):
+        self.reason = reason
+        self.stdout = stdout
+        self.stderr = stderr
+        self.returncode = returncode
+        super(KiwiError, self).__init__(message)
 
 
 class InterfaceDriverError (KiwiError):
-    def __init__(self, status=None):
-        self.status = status
-        super(InterfaceDriverError, self).__init__()
+    pass
+
 
 class FirewallDriverError (KiwiError):
-    def __init__(self, status=None):
-        self.status = status
-        super(FirewallDriverError, self).__init__()
+    pass
+
+
+class UnknownAddressError (KiwiError):
+    pass
+
+
+class UnclaimedAddressError(KiwiError):
+    pass
+
+
+class ClaimFailedError (KiwiError):
+    pass
+
+
+class RefreshFailedError (KiwiError):
+    pass

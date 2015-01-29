3bb51d4f4bbb4bf356fc7f9d2597d3bc01eb4bf2
Lars Kellogg-Stedman <lars@redhat.com>
2015-01-28 22:36:08 -0500

    refactored addresswatch with an iter_events function
    another cleanup on aisle 3!  modified addresswatcher following the model
    in servicewatcher.

--- kiwi/addresswatcher.py
+++ kiwi/addresswatcher.py
@@ -6,11 +6,37 @@ from multiprocessing import Process
 
 import defaults
 
+LOG = logging.getLogger('kiwi.addresswatcher')
 re_address = re.compile('\d+\.\d+\.\d+\.\d+')
 
 
+def iter_events(url, interval=1, recursive=True):
+    '''Produces an inifite stream of events from etcd regarding the given
+    URL.'''
+
+    waitindex = None
+
+    while True:
+        try:
+            params = {'recursive': recursive,
+                      'wait': True,
+                      'waitIndex': waitindex}
+
+            r = requests.get(url, params=params)
+            r.raise_for_status()
+
+            event = r.json()
+            waitindex = event['node']['modifiedIndex'] + 1
+            yield event
+        except Exception as exc:
+            LOG.error('connection failed: %s' % exc)
+            time.sleep(interval)
+
+
 class AddressWatcher (Process):
-    log = logging.getLogger('kiwi.addresswatcher')
+    '''Watches an etcd directory of keys that represent public ip addresses
+    being managed by kiwi, and writes the resulting events to the message
+    queue.'''
 
     def __init__(self,
                  queue,
@@ -25,50 +51,29 @@ class AddressWatcher (Process):
         self.reconnect_interval = reconnect_interval
 
     def run(self):
-        waitindex = None
         url = '%s/v2/keys%s/publicips' % (self.etcd_endpoint,
                                           self.etcd_prefix)
 
-        while True:
-            self.log.debug('watching from waitindex = %s', waitindex)
+        for event in iter_events(url, interval=self.reconnect_interval):
+            LOG.debug('event: %s', event)
 
-            try:
-                r = requests.get(url,
-                                 params={'recursive': 'true',
-                                         'wait': 'true',
-                                         'waitIndex': waitindex})
-            except Exception as exc:
-                self.log.error('connection to %s failed: %s',
-                               url,
-                               exc)
-            else:
-                if r.ok:
-                    event = r.json()
-                    self.log.debug('event: %s', event)
-                    waitindex = event['node']['modifiedIndex'] + 1
+            node = event['node']
+            address = node['key'].split('/')[-1]
 
-                    node = event['node']
-                    address = node['key'].split('/')[-1]
+            if not re_address.match(address):
+                LOG.error('invalid address %s', address)
+                continue
 
-                    if not re_address.match(address):
-                        self.log.error('invalid address %s', address)
-                        continue
+            handler = getattr(self, 'handle_%s' %
+                              event['action'].lower(), None)
 
-                    handler = getattr(self, 'handle_%s' %
-                                      event['action'].lower(), None)
+            # we log missing handlers at debug level because we probably
+            # intentionally have not written a handler for the event.
+            if not handler:
+                LOG.debug('unknown event: %(action)s' % event)
+                continue
 
-                    if not handler:
-                        self.log.debug('unknown event: %(action)s' % event)
-                        continue
-
-                    handler(address, node)
-                    continue
-                else:
-                    self.log.error('request failed (%d): %s',
-                                   r.status_code,
-                                   r.reason)
-
-            time.sleep(self.reconnect_interval)
+            handler(address, node)
 
     def handle_create(self, address, node):
         self.q.put({'message': 'create-address',
@@ -97,13 +102,14 @@ class AddressWatcher (Process):
                     'node': node})
 
 if __name__ == '__main__':
-    import multiprocessing
-    logging.basicConfig(level=logging.DEBUG)
+    from multiprocessing import Queue
+    import pprint
 
-    q = multiprocessing.Queue()
+    logging.basicConfig(level=logging.DEBUG)
+    q = Queue()
     s = AddressWatcher(q)
     s.start()
 
     while True:
         msg = q.get()
-        print 'msg:', msg
+        pprint.pprint(msg)

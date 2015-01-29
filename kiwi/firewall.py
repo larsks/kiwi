827cd9c022ad619fdfe4de96f66e004b95ced75f
Lars Kellogg-Stedman <lars@redhat.com>
2015-01-28 21:32:37 -0500

    added docs to firewall module

--- kiwi/firewall.py
+++ kiwi/firewall.py
@@ -5,12 +5,18 @@ import defaults
 from exc import *
 
 class Firewall (object):
+    '''This is a firewall driver for kiwi, the Kubernetes address manager.
+    This driver operates by creating rules in the `mangle` table that will
+    apply a specific firewall mark to inbound packets.  The driver uses the
+    mangle table in order to match packets before they are modified by the
+    REDIRECT rules generated in the nat table by kube-proxy.'''
 
     log = logging.getLogger('kiwi.firewall')
 
     def __init__(self,
                  fwchain=defaults.fwchain,
                  fwmark=defaults.fwmark):
+
         self.fwchain = fwchain
         self.fwmark = fwmark
         self.rules = set()
@@ -21,75 +27,84 @@ class Firewall (object):
     def cleanup(self):
         self.flush_rules()
 
-    def create_chain(self):
+    def chain_exists(self, chain, table='mangle'):
+        '''Return True if the given chain exists, False otherwise.'''
         ret = subprocess.call([
-                'iptables', '-t', 'mangle',
-                '-S', self.fwchain
-            ])
+            'iptables', '-t', table, '-S', chain])
 
-        if ret == 0:
+        return ret == 0
+
+    def create_chain(self):
+        '''Create self.fwchain if it does not already exist.'''
+
+        if self.chain_exists(self.fwchain):
             return
 
         self.log.info('creating chain %s', self.fwchain)
         try:
             subprocess.check_call([
-                'iptables', '-t', 'mangle',
-                '-N', self.fwchain
-            ])
+                'iptables', '-t', 'mangle', '-N', self.fwchain])
         except subprocess.CalledProcessError as exc:
             raise FirewallDriverError(status=exc)
 
     def flush_rules(self):
+        '''Flush all rules in self.fwchain.'''
+
         self.log.info('flushing all rules from %s',
                       self.fwchain)
         self.rules = set()
         try:
             subprocess.check_call([
-                'iptables', '-t', 'mangle',
-                '-F', self.fwchain])
+                'iptables', '-t', 'mangle', '-F', self.fwchain])
         except subprocess.CalledProcessError as exc:
             raise FirewallDriverError(status=exc)
 
     def rule_for(self, address, service):
+        '''Generate an iptables rule (returned as a tuple) for the given
+        address and service.'''
+
         return tuple(str(arg) for arg in [
-                     '-d', address,
-                     '-p', service['protocol'].lower(),
-                     '--dport', service['port'],
-                     '-m', 'comment',
-                     '--comment', service['id'],
-                     '-j', 'MARK', '--set-mark', self.fwmark
+            '-d', address,
+            '-p', service['protocol'].lower(),
+            '--dport', service['port'],
+            '-m', 'comment',
+            '--comment', service['id'],
+            '-j', 'MARK', '--set-mark', self.fwmark
         ])
 
     def add_service(self, address, service):
+        '''Add a new service to the firewall.'''
+
         rule = self.rule_for(address, service)
         if rule in self.rules:
-            self.log.info('not adding rule for service %s on %s port %d (already exists)',
-                      service['id'], address, service['port'])
+            self.log.info('not adding rule for service %s '
+                          'on %s port %d (already exists)',
+                          service['id'], address, service['port'])
             return
 
-
-        self.log.info('adding firewall rules for service %s on %s port %d',
+        self.log.info('adding firewall rules for service %s '
+                      'on %s port %d',
                       service['id'], address, service['port'])
 
         try:
-            subprocess.check_call((
-                'iptables', '-t', 'mangle', '-A', self.fwchain,
-            ) + rule)
+            subprocess.check_call(('iptables', '-t', 'mangle',
+                                   '-A', self.fwchain) + rule)
         except subprocess.CalledProcessError as exc:
             raise FirewallDriverError(status=exc)
 
         self.rules.add(rule)
 
     def remove_service(self, address, service):
+        '''Remove a service from the firewall.'''
+
         rule = self.rule_for(address, service)
 
-        self.log.info('removing firewall rules for service %s on %s port %d',
+        self.log.info('removing firewall rules for service %s '
+                      'on %s port %d',
                       service['id'], address, service['port'])
         self.rules.remove(rule)
         try:
-            subprocess.check_call((
-                'iptables', '-t', 'mangle', '-D', self.fwchain,
-            ) + rule)
+            subprocess.check_call(('iptables', '-t', 'mangle',
+                                   '-D', self.fwchain) + rule)
         except subprocess.CalledProcessError as exc:
             raise FirewallDriverError(status=exc)
-
